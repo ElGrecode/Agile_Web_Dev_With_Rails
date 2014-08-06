af7d7428e6ec7262cec4101ea74795ac33ace97d
narwhaldo <narwhaldo@gmail.com>
2014-08-05 19:47:52 -0700

    change in model schema to reflect adding to quantity of line_items instead of creating new ones

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,16 @@
 class Cart < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
+
+  def add_product(product_id)
+    current_item = line_items.find_by(product_id: product_id)
+    if current_item
+      # Something is there
+      current_item.quantity += 1
+    else
+      # Something new
+      current_item = line_items.build(product_id: product_id)
+    end
+    current_item
+  end
+
 end
